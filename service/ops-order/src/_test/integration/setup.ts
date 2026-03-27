db0c89627983152115cc2c114db091218c9c07dc
Kirill Tatarenko <kirill.tatarenko@rockbitegames.com>
2026-03-27 15:40:45 +0400

    process: wrote a couple of tests, still in the process

--- service/ops-order/src/_test/integration/setup.ts
+++ service/ops-order/src/_test/integration/setup.ts
@@ -1,15 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-
 import { prisma } from "../../config/PrismaConfig";
 import { intercept } from "../util";
 
-export let prismaClient: PrismaClient = null;
-
 beforeAll(async () => {
-    prismaClient = new PrismaClient({ datasourceUrl: process.env.__PG_URI, transactionOptions: { maxWait: 11111, timeout: 11111 } });
-    await prismaClient.$connect();
 
-    exports.prisma = prismaClient;
 });
 
 beforeEach(async () => {
@@ -17,10 +10,9 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    await prismaClient.$executeRawUnsafe(`TRUNCATE TABLE "idempotency", "order", "order_item", "outbox", "processed_event" RESTART IDENTITY CASCADE;`);
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE "idempotency", "order", "order_item", "outbox", "processed_event" RESTART IDENTITY CASCADE;`);
 });
 
 afterAll(async () => {
-    await prismaClient.$disconnect();
     await prisma.$disconnect();
 });
--- service/ops-order/src/_test/util.ts
+++ service/ops-order/src/_test/util.ts
@@ -1,9 +1,35 @@
+import { RecordMetadata } from "kafkajs";
+import { kafkaProducer } from "../broker/producer/KafkaProducer";
+import { CreateOrderRequest } from "../schema/ExternalSchemas";
+import { EventEmit } from "../type/Type";
+
 export const SPY: {
-
+    send: jest.SpyInstance
 } = {
+    send: null
+};
 
+export const idempotencyKey = "some-idempotency-key";
+export const createOrder: CreateOrderRequest = {
+    customerId: "fd61c63a-2383-4812-bc9e-e2742d52f48e",
+    currency: "USD",
+    items: [
+        { sku: "sku-1", quantity: 23.14, unitPrice: 15.56 },
+        { sku: "sku-2", quantity: 90, unitPrice: 14 },
+        { sku: "sku-3", quantity: 2, unitPrice: 70.50 }
+    ],
+    shippingAddress: {
+        country: "USA",
+        city: "Minnesota",
+        addressLine1: "Saint Lois Str. 15"
+    }
 };
 
 export function intercept(): void {
+
+    SPY.send = jest.spyOn(kafkaProducer, "send").mockImplementation(
+        async (data: EventEmit, topic: string): Promise<RecordMetadata[]> => { return null }
+    );
+
     
 }
